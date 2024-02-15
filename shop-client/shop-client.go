e2f3ce79046112f3f2abc63d3301ac2c0b894b96
Ivan Pysmennyi <ivanpysmenni@gmail.com>
2024-02-15 22:32:54 +0100

    Start work on shop-client app

--- shop-client/shop-client.go
+++ shop-client/shop-client.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	shopAddress := flag.String("h", "127.0.0.1", "Shop ip address")
+	shopPort := flag.Int("p", 8080, "Shop port")
+	appName := flag.String("-n", "undefined-client", "Client name")
+
+	fmt.Printf("My name is %s. I will go to shop by this address %s:%d", *appName, *shopAddress, shopPort)
+
 }
+
+// 1. Read parameters with information about server(shop)
+// 2. Output information about this client
+// 3. Make a request to server for get information about available products
+// 4. Buy one random product
+// 5. Sleep for several seconds(random from 1 to 5)
+// 6. Repeat steps 3,4,5 till no products are available
--- shop-manager/shop-manager.go
+++ shop-manager/shop-manager.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// 1. Read parameters with information about server(shop)
+	// 2. Output information about this manager
+	// 3. Make a request to server for get information about how many products in shop and print it
+	// 4. Sleep for several seconds(random from 1 to 5)
+	// 5. Repeat steps 3,4 till no products are available
+
 	fmt.Println("Hello, World!")
 }
